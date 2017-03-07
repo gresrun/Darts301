3008e2afc64659b07a953cb82fa49b3b1093c439
Greg Haines <greg@greghaines.net>
2017-03-07 00:41:48 -0800

    Add basic validation to who's playing view

--- App/Darts301/Darts301/WhosPlayingViewController.swift
+++ App/Darts301/Darts301/WhosPlayingViewController.swift
@@ -6,14 +6,16 @@
 //  Copyright © 2017 Aly & Friends. All rights reserved.
 //
 
+import SwiftValidator
 import UIKit
 
-class WhosPlayingViewController : UIViewController, UITextFieldDelegate {
+class WhosPlayingViewController : UIViewController, UITextFieldDelegate, ValidationDelegate {
 
     @IBOutlet weak var player1NameField: UITextField!
     @IBOutlet weak var player2NameField: UITextField!
     @IBOutlet weak var skipButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
+    private let validator = Validator()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,11 @@ class WhosPlayingViewController : UIViewController, UITextFieldDelegate {
         skipButton.layer.borderColor = Colors.player1Color.cgColor
         skipButton.layer.borderWidth = 2.0
         Utils.addShadow(to: startButton)
+        player1NameField.layer.cornerRadius = 4.0
+        player2NameField.layer.cornerRadius = 4.0
+        validator.registerField(player1NameField, rules: [RequiredRule()])
+        validator.registerField(player2NameField, rules: [RequiredRule()])
+        clearValidationErrors()
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -34,22 +41,72 @@ class WhosPlayingViewController : UIViewController, UITextFieldDelegate {
         }
     }
 
+    @IBAction func startTapped(_ sender: UIButton) {
+        validator.validate(self)
+    }
+
+    private func clearValidationErrors() {
+        for field in [player1NameField, player2NameField] {
+            if let field = field {
+                clearValidationError(field)
+            }
+        }
+    }
+
+    private func clearValidationError(_ field: UITextField) {
+        field.layer.borderColor = UIColor.clear.cgColor
+        field.layer.borderWidth = 0.0
+    }
+
+    private func addValidationError(_ field: UITextField) {
+        field.layer.borderColor = UIColor.red.cgColor
+        field.layer.borderWidth = 1.0
+    }
+
+    private func validateField(_ field: UITextField) {
+        validator.validateField(field) { error in
+            if let _ = error {
+                addValidationError(field)
+            } else {
+                clearValidationError(field)
+            }
+        }
+    }
+
     // MARK: UITextFieldDelegate
     func textFieldDidBeginEditing(_ textField: UITextField) {
         // Do nothing
     }
 
     func textFieldDidEndEditing(_ textField: UITextField) {
+        validateField(textField)
         textField.resignFirstResponder()
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if (textField === player1NameField) {
+            validateField(textField)
             player2NameField.becomeFirstResponder()
         } else if (textField === player2NameField) {
             textField.resignFirstResponder()
-            performSegue(withIdentifier: Segues.startGame, sender: textField)
+            validator.validate(self)
         }
         return true;
+    }
+
+    // MARK: ValidationDelegate
+    func validationSuccessful() {
+        clearValidationErrors()
+        performSegue(withIdentifier: Segues.startGame, sender: self)
+    }
+
+    func validationFailed(_ errors:[(Validatable, ValidationError)]) {
+        clearValidationErrors()
+        // Highlight fields with errors in red
+        for (field, _) in errors {
+            if let field = field as? UITextField {
+                addValidationError(field)
+            }
+        }
     }
 }
