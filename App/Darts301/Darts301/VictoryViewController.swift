0ccc46ea63098dfa251995465cfa75889943596a
Greg Haines <greg@greghaines.net>
2017-10-04 00:29:44 -0700

    Remove old random GIF URL

--- App/Darts301/Darts301/VictoryViewController.swift
+++ App/Darts301/Darts301/VictoryViewController.swift
@@ -12,8 +12,6 @@ import UIImage_animatedGif
 import UIKit
 
 class VictoryViewController : UIViewController, GADInterstitialDelegate {
-    private static let randomGifURL =
-        "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=celebrate&rating=g&fmt=json"
 
     @IBOutlet weak var celebrateImageView: UIImageView!
     @IBOutlet weak var celebrateImageViewHeight: NSLayoutConstraint!
