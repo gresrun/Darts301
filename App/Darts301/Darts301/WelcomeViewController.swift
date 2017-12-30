aaad4ad45abd12fa39ef9d7f50a4c54471ad4e09
Greg Haines <greg@greghaines.net>
2017-12-29 21:06:13 -0500

    Add fun animation to Welcome VC

--- App/Darts301/Darts301/WelcomeViewController.swift
+++ App/Darts301/Darts301/WelcomeViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class WelcomeViewController : UIViewController {
 
+    @IBOutlet weak var gradientView: GradientView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabelVerticalCenterConstraint: NSLayoutConstraint!
     @IBOutlet weak var rulesButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = Colors.player1Color
         startButton.layer.cornerRadius = 8.0
         rulesButton.layer.cornerRadius = 8.0
         rulesButton.layer.borderColor = UIColor.white.cgColor
@@ -22,6 +26,27 @@ class WelcomeViewController : UIViewController {
         Utils.addShadow(to: startButton)
         rulesButton.setTitle(NSLocalizedString("RULES_BUTTON", value: "Rules", comment: "Button that, when pressed, will direct the user to the rules of the game."), for: .normal)
         startButton.setTitle(NSLocalizedString("START_GAME_BUTTON", value: "Start new game", comment: "Button that, when pressed, will start a new game."), for: .normal)
+        gradientView.alpha = 0.0;
+        rulesButton.alpha = 0.0;
+        startButton.alpha = 0.0;
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.view.removeConstraint(self.titleLabelVerticalCenterConstraint)
+        self.view.addConstraint(NSLayoutConstraint(item: self.titleLabel,
+                                                         attribute: .top,
+                                                         relatedBy: .equal,
+                                                         toItem: self.topLayoutGuide,
+                                                         attribute: .bottom,
+                                                         multiplier: 1,
+                                                         constant: 120))
+        UIView.animate(withDuration: 0.8) {
+            self.gradientView.alpha = 1.0;
+            self.rulesButton.alpha = 1.0;
+            self.startButton.alpha = 1.0;
+            self.view.layoutIfNeeded()
+        }
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
